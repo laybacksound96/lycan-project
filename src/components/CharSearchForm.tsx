3b1411501978bb3c30157a4b29cdc464cd853ea2
412 <85489519+laybacksound96@users.noreply.github.com>
2023-02-20 12:51:27 +0900

    Error

--- src/components/CharSearchForm.tsx
+++ src/components/CharSearchForm.tsx
@@ -1,13 +1,30 @@
 import { useForm } from "react-hook-form";
-
+import { useQuery } from "react-query";
+import { fetchCharInfo } from "../api";
 function CharSearchForm() {
   const { register, handleSubmit, setValue } = useForm();
-  const onSubmit = (aa: any) => {
-    console.log(aa);
+
+  interface ICharacterInfo {
+    ["ServerName"]: string;
+    ["CharacterName"]: string;
+    ["CharacterLevel"]: number;
+    ["CharacterClassName"]: string;
+    ["ItemAvgLevel"]: string;
+    ["ItemMaxLevel"]: string;
+  }
+
+  const OnSubmit = async (input: any) => {
+    const { data } = useQuery<ICharacterInfo[]>(
+      "CharInfo",
+      fetchCharInfo("asd")
+    );
+    console.log(data);
+
     setValue("Search", "");
   };
+
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(OnSubmit)}>
       <input {...register("Search")} />
       <button>aa</button>
     </form>
