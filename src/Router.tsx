e56ae840f12ed53b1b34c87730e85f03594a4221
412 <laybacksound96@gmail.com>
2023-05-08 13:44:56 +0900

    auth부분 다시짜기

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,7 +1,5 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import CheckBox from "./routes/CheckBox";
-import Home from "./routes/Home";
-import Login from "./routes/Login";
 
 function Router() {
   return (
